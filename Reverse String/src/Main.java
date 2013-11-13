9aca9f10441f19dec6d3823fbf5f9e93e697c44f
dhiraj <dhiraj@dhiraj-PC>
2013-11-13 09:53:09 +0530

    first time

---
+++ Reverse String/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="raman";
+		int length=name.length();
+		for(int i=length-1;i>=0;i--) {
+			char c= name.charAt(i);
+			System.out.print(c);
+		}
+	}
+
+}
